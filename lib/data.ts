5d0fc02cea48cdc775b0a0e98e223cdf8f2101ab
fantaraymen <fantaraymen943@gmail.com>
2026-06-08 03:48:35 +0100

    fix data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -25,7 +25,7 @@ export const testimonials = [
     name: "sonia Ben Ali",
     location: "Tunis",
     rating: 5,
-    comment: "Lberah barka wesletli l banquette, ya3tikom l saha 3al service et surtout qualité du produit 💯",
+    comment: "Lberah barka wesletli l banquette, ya3tikom l saha 3al service et surtout qualité du produit",
     avatar: "https://i.ibb.co/ZRrWW5Jz/505058216-2839404319578329-4974240031999761051-n.jpg"
   },
   {
@@ -33,5 +33,23 @@ export const testimonials = [
     name: "Soumaya Nabli",
     location: "Sousse",
     rating: 5,
-    comment: "❤️السلام عليكم توا وصلتني الكوموند ربي يباركلكم",
-    avatar: "https://i.ibb.co/DD394CF0/489286305-9407843442667828-49835142405171
+    comment: "السلام عليكم توا وصلتني الكوموند ربي يباركلكم",
+    avatar: "https://i.ibb.co/DD394CF0/489286305-9407843442667828-4983514240517174301-n.jpg"
+  },
+  {
+    id: 3,
+    name: "Amira Gharbi",
+    location: "Sfax",
+    rating: 5,
+    comment: "5dhit men aandkom l kisen w lahkika rapport qualité prix impéccable",
+    avatar: "https://i.ibb.co/d4z3Z124/545196055-17988283814843996-1652015810091033581-n.jpg"
+  },
+  {
+    id: 4,
+    name: "Youssef Mansour",
+    location: "Bizerte",
+    rating: 5,
+    comment: "ghalta le berasmi yaatikom l saha a khouya",
+    avatar: "https://i.ibb.co/p6wM2Njq/download.jpg"
+  }
+]
